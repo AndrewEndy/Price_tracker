d4f0bd2cb9a8422460ef63d0315c442387189bf4
Andrew <andrijpastuh@gmail.com>
2024-07-30 22:21:01 +0300

    Add a new method
    -Add the get_products method
    
    -Add annotations for most methods

--- db_hadlers/db_class.py
+++ db_hadlers/db_class.py
@@ -12,11 +12,13 @@ class DatabaseBot:
         self.db = await aiosqlite.connect(self.db_file)
         return self
 
+
     async def __aexit__(self, exc_type, exc_value, traceback):
         await self.db.close()
         
         
     async def on_start_up(self):
+        '''Метод який, при старті бота, створює таблиці в бд'''
 
         await self.db.execute('''
             CREATE TABLE IF NOT EXISTS Users (
@@ -39,6 +41,7 @@ class DatabaseBot:
         
 
     async def check_user(self, telegram_id: int):
+        '''Метод який перевіряє чи є користувач в бд'''
         async with self.lock:
             async with self.db.execute("SELECT telegram_id FROM Users WHERE telegram_id = ?", (telegram_id,)) as cursor:
                 user_exist = await cursor.fetchone()
@@ -46,6 +49,17 @@ class DatabaseBot:
             
     
     async def add_user(self, telegram_id: int, username: str):
+        '''Метод який додає нового користувача в бд'''
         async with self.lock:
             await self.db.execute("INSERT INTO Users (telegram_id, username) VALUES (?, ?)", (telegram_id, username))
             await self.db.commit()
+            
+    
+    async def get_products(self, telegram_id: int):
+        '''Метод для отримання всіх товарів користувача'''
+        async with self.lock:
+            async with self.db.execute("SELECT * FROM Products WHERE telegram_id = ?", (telegram_id,)) as cursor:
+                products = await cursor.fetchall()
+                return products
+
+
