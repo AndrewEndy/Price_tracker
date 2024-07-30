815328ecc1d48fe97afda05be229055f7401ff86
Andrew <andrijpastuh@gmail.com>
2024-07-30 22:16:42 +0300

    Add new features
    -Add handling of the "view my items" command
    
    -Add processing of the command "Add new product"
    
    -Add callback_date processing that returns to the main menu

--- handlers/user_handler.py
+++ handlers/user_handler.py
@@ -1,3 +1,4 @@
+import asyncio
 from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
@@ -5,15 +6,17 @@ from aiogram.types import ReplyKeyboardRemove
 from aiogram.fsm.context import FSMContext
 from create_bot import admins, tg_db
 from keyboards.reply_keyboard import main_kb
+from keyboards.inline_keyboard import inline_kb_store, back_inline_kb
 from db_hadlers.db_class import DatabaseBot
-#from keyboards.inline_keyboards import base_link_kb
+
+
 
 user_router = Router()
 
 
 @user_router.message(CommandStart())
 async def cmd_start(message: Message, state: FSMContext):
-    state.clear()
+    await state.clear()
     
     async with DatabaseBot(tg_db) as database:
         user_exists = await database.check_user(message.from_user.id)
@@ -25,9 +28,38 @@ async def cmd_start(message: Message, state: FSMContext):
     
     await message.answer(f'Ось головне меню!', reply_markup=main_kb(message.from_user.id))
     
+    
+@user_router.message(F.text == '🛍 Додати новий товар')
+async def cmd_add_new_product(message:Message):
+    msg = await message.answer('Виберіть онлайн магази🛒', reply_markup=ReplyKeyboardRemove())
+    asyncio.sleep(0.1)
+    await msg.delete()
+
+    await message.answer(f'Виберіть онлайн магази🛒', reply_markup=inline_kb_store())
+    
+
+@user_router.message(F.text == '👀 Переглянути мої товари')
+async def cmd_my_products(message: Message):
+    async with DatabaseBot(tg_db) as database:
+        products = await database.get_products(message.from_user.id)
+        
+    if products:
+        pass
+    else:
+        await message.answer(f'У вас немає доданих товарів😔', reply_markup=main_kb(message.from_user.id))
+
 
 @user_router.message(F.text == '❓ Про нас')
 async def cmd_about_us(message: Message):
     await message.answer(f'Бот створений для спостереженям за цінами в різних онлайн магазинах')
     await message.answer(f'Бот поки що знаходиться в стадії розробки, якщо ви знайшли якись баг повідомте мене про це будь ласка'
                          , reply_markup=main_kb(message.from_user.id))
+    
+
+@user_router.callback_query(F.data == 'back_home')
+async def cmd_on_main(data: CallbackQuery, state: FSMContext):
+    await state.clear()
+    await data.answer()
+    await data.message.delete()
+    await data.message.answer(f'Ось головне меню!', reply_markup=main_kb(data.from_user.id))
+    
